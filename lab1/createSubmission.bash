38b134d3c80da442f8a197026db75513bde07265
Daniel Kaehn <kaehnd@msoe.edu>
2021-03-09 20:11:00 -0600

    Some more refactoring

--- lab1/createSubmission.bash
+++ lab1/createSubmission.bash
@@ -1,8 +1,7 @@
-#Script to create pdf and zip submission from source files in 
+#Script to create pdf and zip submission from source files in
 #src/ and include/ directories
 #Takes one parameter, the name (without extension) of both output files
 
-declare -A inc #include files already processed
 declare -A src #src files already processed
 
 outFilename=$1
@@ -12,14 +11,11 @@ submitFiles="./src/main.cpp"
 src[$submitFiles]=1
 
 #Add each include/ header file if files exist in include/ dir
-#add the corresponding cpp files in order if they exist 
+#and add the corresponding cpp files in order if they exist
 DIR=./include
 if [ "$(ls -A $DIR)" ]; then
 
-  for f in $DIR/*
-  do
-    inc[$f]=1
-
+  for f in $DIR/*; do
     submitFiles="$submitFiles $f"
 
     incFilename=$(basename -- "$f")
@@ -36,8 +32,7 @@ fi
 DIR=./src
 if [ "$(ls -A $DIR)" ]; then
 
-  for f in $DIR/*
-  do
+  for f in $DIR/*; do
     if [ ${src["$f"]} -ne 1 ]; then
       src[$f]=1
       submitFiles="$submitFiles $f"
@@ -51,7 +46,7 @@ submitFiles="$submitFiles ./Makefile"
 echo -e "Creating $outFilename.pdf...\n"
 
 a2ps --pro=color --toc -E -1 --header="$outFilename" --line-numbers=1 \
--l 90 -T 4 -o - $submitFiles  | ps2pdfwr - "$outFilename".pdf
+  -l 90 -T 4 -o - $submitFiles | ps2pdfwr - "$outFilename".pdf
 
 echo -e "\n\nCreating $outFilename.zip... \n"
 zip "$outFilename".zip $submitFiles
